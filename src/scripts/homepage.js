e0cb96e266b07596c1e1d6396f6473e694012234
frost3dWave <181492974+frost3dWave@users.noreply.github.com>
2025-04-08 13:29:10 +0530

    Finished homepage
    - updated the text content & added buttons to a seperate div for ease of styling
    - Refactored entire CSS file for better readability & reduced complexity
    
      -- Simplified styles for easier maintenance
      -- Removed unnecessary rules & redundant code
      -- Improved organization for better clarity

--- src/scripts/homepage.js
+++ src/scripts/homepage.js
@@ -9,26 +9,31 @@ export function homePage(){
     
     const topHeading = document.createElement("h2");
     topHeading.classList.add("restaurant-title");
-    topHeading.textContent = "Restaurant headline";
+    topHeading.textContent = "Enjoy Classic Fast Food";
     
     const pageDescription = document.createElement("p");
     pageDescription.classList.add("restaurant-desc"); 
-    pageDescription.textContent = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum at dolore facilis qui? Quaerat illum qui ullam quas iure ducimus eaque! Vero aliquam voluptatibus quaerat libero dolore non quidem corporis";
+    pageDescription.textContent = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum at dolore facilis qui? Quaerat illum qui ullam quas iure ducimus eaque!";
     
+    const buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("button-container");
+
     const buyBtn = document.createElement("button");
     buyBtn.textContent = "Buy Now";
     
     const aboutBtn = document.createElement("button");
     aboutBtn.textContent = "About US";
     
+    buttonContainer.appendChild(buyBtn);
+    buttonContainer.appendChild(aboutBtn);
+    
     newSection.appendChild(topHeading);
     newSection.appendChild(pageDescription);
-    newSection.appendChild(buyBtn);
-    newSection.appendChild(aboutBtn);
+    newSection.appendChild(buttonContainer);
     
     const homePageImage = document.createElement("img");
     homePageImage.src = burgerImage;
     
-    divContent.appendChild(newSection);
     divContent.appendChild(homePageImage);
+    divContent.appendChild(newSection);
 };
