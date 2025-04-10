7c316b98f6c48d19617b6be37aab6a3f5ec86302
frost3dWave <181492974+frost3dWave@users.noreply.github.com>
2025-04-10 19:42:57 +0530

    updated with tab switching logic
    - included reset & variables css files for use in all other modules
    - included tab switching logic to travel between different pages of the site using navigation buttons

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,18 @@
+import "../styles/variables.css";
 import "../styles/reset.css";
 import { homePage } from "./homepage.js";
+import { menu } from "./menu.js";
+import { about } from "./about.js";
 
-homePage();
+
+window.onload = homePage;
+
+const headerTitle = document.querySelector("h1");
+const homepage = document.querySelector(".home");
+const menupage = document.querySelector(".menu");
+const aboutpage = document.querySelector(".about");
+
+headerTitle.addEventListener("click", homePage);
+homepage.addEventListener("click", homePage);
+menupage.addEventListener("click", menu);
+aboutpage.addEventListener("click", about);
